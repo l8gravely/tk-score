623f9db11b0ae91bfd9fb34b7e9ed0d88182916a
John Stoffel <john@stoffel.org>
2014-12-23 17:08:13 -0500

    - Initial setup of OO Perl to simplify how matches are handled.

---
+++ Schedule.pm
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+# Initial stab at OO tk-score modules to make handing crap simpler.
+
+package Schedule;
+
+sub new {
+  my $class = shift;
+  my $self = {
+	     };
+  bless $self, $class;
+  return $self;
+}
+
+sub setvar {
+    my ( $self, $var, $val ) = @_;
+    $self->{$var} = $val if defined($val);
+    return $self->{$var};
+  }
+
+sub getvar {
+    my( $self, $var ) = @_;
+    return $self->{$var};
+  }
+
+1;
